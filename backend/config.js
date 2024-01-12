af4c40a37cb3f6529ea74c11db965fd757c84b4f
UdaybhaskarTalari <20a91a1252@aec.edu.in>
2024-01-12 11:39:53 +0530

    s3_multipart_updated

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 exports.globals = {
-    "AWS-BUCKET-NAME": "bucketfromnode1", // Replace with your bucket 
-    "ACCESS-ID": "AKIAZVCNVINWBWOFXLWZ", // Replace with your access key id 
-    "AWS-SECRET-KEY": "yA5dsTWmOsu94ZVnJ0gQd7dIna8lejH55NKnmv8v", // Replace with your secret access key 
+    "AWS-BUCKET-NAME": "", // Replace with your bucket 
+    "ACCESS-ID": "", // Replace with your access key id 
+    "AWS-SECRET-KEY": "", // Replace with your secret access key 
     "signatureVersion": 'v4'
 }
