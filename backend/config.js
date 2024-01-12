cd59b804570286c25e9e64d545bf1669cad11041
UdaybhaskarTalari <20a91a1252@aec.edu.in>
2024-01-12 11:37:50 +0530

    s3_multipart

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+exports.globals = {
+    "AWS-BUCKET-NAME": "bucketfromnode1", // Replace with your bucket 
+    "ACCESS-ID": "AKIAZVCNVINWBWOFXLWZ", // Replace with your access key id 
+    "AWS-SECRET-KEY": "yA5dsTWmOsu94ZVnJ0gQd7dIna8lejH55NKnmv8v", // Replace with your secret access key 
+    "signatureVersion": 'v4'
+}
